948ea31807a07b12edde366af3a0fb00a7b39147
grant-walters <31633116+grant-walters@users.noreply.github.com>
2021-01-18 00:41:17 -0800

    Content additions

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,7 +4,9 @@ class Home extends React.Component {
 	render() {
 		return (
 			<div class='content'>
-				Test
+				<p class='heading'>Grant Walters</p>
+				<hr>
+				<p>Hello, I am Grant Walters, a computer scientist, Star Wars enthusiast, and rock climber. I spend a lot of my free time between playing games, watching shows, coding, rock climbing, or just talking with friends. This website serves two purposes. One is to serve as a professional portfolio, displaying my projects, my resume, and contact information. It will also serve as a website where I host any personal projects I am working on, although a lot of that will probably go on my other site.</p>
 			</div>
 		);
 	}
