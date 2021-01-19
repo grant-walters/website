40fddbd3a0d5046ac07921dec507744c4af7deed
grant-walters <31633116+grant-walters@users.noreply.github.com>
2021-01-18 20:39:04 -0800

    omg plz send help

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -13,6 +13,8 @@ class Home extends React.Component {
 					<p>Hello, I am Grant Walters, a computer scientist, Star Wars enthusiast, and rock climber. I spend a lot of my free time between playing games, watching shows, coding, rock climbing, or just talking with friends. This website serves two purposes. One is to serve as a professional portfolio, displaying my projects, my resume, and contact information. It will also serve as a website where I host any personal projects I am working on, although a lot of that will probably go on my other site.</p>
 				</div>
 				<ContentSquare title="Test" description="Testing this out."/>
+				<ContentSquare title="Test" description="Testing this out."/>
+				<ContentSquare title="Test" description="Testing this out."/>
 			</div>
 		);
 	}
